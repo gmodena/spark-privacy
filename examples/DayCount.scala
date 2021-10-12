fa68273c43f48afa3ca7727fbb5e55e3e0978712
gmodena <gmodena@wikimedia.org>
2021-10-12 14:15:12 +0200

    Add README

--- examples/DayCount.scala
+++ examples/DayCount.scala
@@ -1,9 +1,12 @@
+// A distributed implementation of
+// https://github.com/google/differential-privacy/tree/main/examples/java#count-visits-by-day-of-week
+
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions.{col, count, udaf}
 import privacy.spark.{BoundContribution, PrivateCount}
 
 object DayCount extends App {
-  final private val DayDataCsv = "day_data.csv"
+  final private val DayDataCsv = "week_data.csv"
   final private val DayDataDelimiter = ","
 
   val spark:SparkSession = SparkSession.builder()
@@ -12,13 +15,12 @@ object DayCount extends App {
   import spark.implicits._
 
   val epsilon = Math.log(3)
-  val maxContributions = 5
+  val maxContributions = 3
   val maxContributionsPerPartition = 1
 
   val dataFrame = spark.read
     .option("delimiter", DayDataDelimiter)
-    .option("header", "true")
-    .csv(DayDataCsv)
+    .option("header", "true").csv(DayDataCsv)
 
   val privateCount = new PrivateCount[Long](epsilon, maxContributions)
   val privateCountUdf = udaf.register(privateCount)
@@ -26,6 +28,6 @@ object DayCount extends App {
   // Restrict `maxContributionsPerPartition` of `VisitoryId` per `Day`, and `maxContributions` in total
   dataFrame.transform(BoundContribution("Day", "VisitorId", maxContributions, maxContributionsPerPartition))
     .groupBy("Day")
-    .agg(privateCountUdf($"VisitorId"))
+    .agg(privateCountUdf($"VisitorId") as "private_count", count($"VisitorId") as "non_private_count" )
     .show
 }
